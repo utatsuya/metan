fade54603efe094685edeb0450521b2f49fc62f7
Tatsuya Uehara <uehara.tatsuya@gmail.com>
2017-03-22 20:55:28 +0900

    テストコード追記

--- python/metan/tests/test_wrapper.py
+++ python/metan/tests/test_wrapper.py
@@ -21,6 +21,27 @@ class TestScene(unittest.TestCase):
         print 'm.attr("t").tx.get()', m.attr("t").tx.get()
         print 'm.attr("t").attr("x").get()', m.attr("t").attr("x").get()
         print 'm.attr("t").attr("tx").get()', m.attr("t").attr("tx").get()
+        print "="*20
+        print 'm.m.get()', m.m.get()
+        print 'm.pim.get()', m.pim.get()
+        print 'm.pim[0].get()', m.pim[0].get()
+        print 'm.attr("pim").get()', m.attr("pim").get()
+        print 'm.attr("pim")[0].get()', m.attr("pim")[0].get()
+        print 'm.attr("pim[0]").get()', m.attr("pim[0]").get()
 
     def runTest(self):
         self.test_getset()
+
+
+class TestScene2(unittest.TestCase):
+
+    def test_getset(self):
+        cmds.file(new=True, f=True)
+        cube = cmds.polyCube()[0]
+        print cube
+        m = metan.M("pCube1")
+        print 'm.t.x.get()', m.t.x.get()
+
+    def runTest(self):
+        self.test_getset()
+
